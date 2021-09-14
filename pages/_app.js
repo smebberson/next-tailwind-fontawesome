1bc964df34ecb1c51832ac531a7775ec5f400128
Scott Mebberson <scott@scottmebberson.com>
2021-09-14 05:14:16 +0000

    Added Font Awesome.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,9 @@
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '../node_modules/@fortawesome/fontawesome-svg-core/styles.css';
+
 import '../styles/globals.css';
+
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
