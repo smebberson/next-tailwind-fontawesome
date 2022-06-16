0dc8c55d09021f09caf4f0898a7cbe2b46b44999
smebberson <74628+smebberson@users.noreply.github.com>
2022-06-16 13:15:04 +0930

    Removed babel-plugin-macros.

--- pages/components/header.jsx
+++ pages/components/header.jsx
@@ -1,24 +1,26 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
+import { faCode } from '@fortawesome/free-solid-svg-icons/faCode';
+import { faSwatchbook } from '@fortawesome/free-solid-svg-icons/faSwatchbook';
+import { faRocket } from '@fortawesome/free-solid-svg-icons/faRocket';
 
 const features = [
     {
         description:
             'Comes pre-installed with Font Awesome, Next.js, Prettier and Tailwind CSS.',
-        icon: solid('code'),
+        icon: faCode,
         title: 'Development made-easy',
     },
     {
         description:
             'Combine Font Awesome and Tailwind CSS to create great looking pages.',
-        icon: solid('swatchbook'),
+        icon: faSwatchbook,
         title: 'Stylish looks',
     },
 
     {
         description:
             'Take advantage of Next.js for lightning fast pages out of the box.',
-        icon: solid('rocket'),
+        icon: faRocket,
         title: 'Fast performance',
     },
 ];
